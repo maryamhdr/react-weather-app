4243447c924ebedbda39b0c884304b0978251ebe
Maryam Heydarian <maryamhdr76@gmail.com>
2023-12-28 19:24:20 +0330

    add autoFocus on search input

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -6,13 +6,12 @@ const Searchbar = () => {
         <form className="flex-container">
             <div className="search-input-container">
                 <i className="fa-solid fa-magnifying-glass search-icon"></i>
-                <input
-                    className="search-input"
-                    type="search"
-                    placeholder="Search for places..."
-                    name="search"
-                    id="search-input"
-                />
+                <input className="search-input"
+                       type="search"
+                       placeholder="Search for places..."
+                       name="search"
+                       id="search-input"
+                       autoFocus="on"/>
             </div>
             <button type="submit" className="search-btn" id="search-btn">
                 <i className="fa-solid fa-location-dot"></i>
