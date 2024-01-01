87ff98e0935b3f95a05d38f7cd796ccedc93ed35
Maryam Heydarian <maryamhdr76@gmail.com>
2024-01-01 15:25:19 +0330

    enable search engine

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,22 +1,27 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import "./searchbar.css";
 
 const Searchbar = ({onDataReady}) => {
-    const [city, setCity] = useState("New York");
-    const apiKey = '99b8f9330a1bfba3a85e523fd3c2e528';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const [city, setCity] = useState("");
+    const apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
 
-    useEffect(() => {
+    const handleInputChange = (event) => {
+        setCity(event.target.value);
+    };
+
+    const search = (event) => {
+        event.preventDefault();
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then((response) => {
-            if(onDataReady) {
+            if (onDataReady) {
                 onDataReady(response.data);
             }
-        })
-    }, []);
+        });
+    };
 
     return (
-        <form className="flex-container">
+        <form className="flex-container" onSubmit={search}>
             <div className="search-input-container">
                 <i className="fa-solid fa-magnifying-glass search-icon"></i>
                 <input className="search-input"
@@ -24,7 +29,8 @@ const Searchbar = ({onDataReady}) => {
                        placeholder="Search for places..."
                        name="search"
                        id="search-input"
-                       autoFocus="on"/>
+                       autoFocus="on"
+                       onChange={handleInputChange}/>
             </div>
             <button type="submit" className="search-btn" id="search-btn">
                 <i className="fa-solid fa-location-dot"></i>
