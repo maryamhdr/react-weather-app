65095033616199e4d9cf3f1cf1064714ad0eb736
Maryam Heydarian <maryamhdr76@gmail.com>
2024-01-04 10:19:00 +0330

    modify some codes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/footer/Footer";
 
 export default function App() {
     const [weatherData, setWeatherData] = useState({isDataReady: false});
-    const [showForecas, setShowForecast] = useState(false);
+    const [showForecast] = useState(false);
 
     const onDataReady = (data) => {
         setWeatherData({
@@ -31,7 +31,7 @@ export default function App() {
                 <Searchbar onDataReady={onDataReady}/>
                 {weatherData.isDataReady && <>
                     <WeatherDataView weatherData={weatherData}/>
-                    {showForecas && <ForecastList/>}
+                    {showForecast && <ForecastList/>}
                 </>}
                 {!weatherData.isDataReady && <EmptyState/>}
                 <Footer/>
